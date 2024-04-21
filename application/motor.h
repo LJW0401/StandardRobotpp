0a8bcdac84662d409851827633f25c17a0832242
小企鹅 <1357482552@qq.com>
2024-04-21 21:54:43 +0800

    将dji电机的控制独立出来

--- application/motor.h
+++ application/motor.h
@@ -31,12 +31,9 @@
             (ecd) += ECD_RANGE; \
     }
 
-#define DJI_GM6020_ECD_TO_RAD \
-    0.000766990394f  // (2*pi/8192) 电机编码器值转换为弧度
-#define DJI_GM3508_RPM_TO_OMEGA \
-    0.0055115661f  // (1/60*2*pi/19) m3508(减速比19:1) (rpm)->(rad/s)
-#define DJI_GM2006_RPM_TO_OMEGA \
-    0.0029088821f  // (1/60*2*pi/36) m2006(减速比36:1) (rpm)->(rad/s)
+#define DJI_GM6020_ECD_TO_RAD 0.000766990394f  // (2*pi/8192) 电机编码器值转换为弧度
+#define DJI_GM3508_RPM_TO_OMEGA 0.0055115661f  // (1/60*2*pi/19) m3508(减速比19:1) (rpm)->(rad/s)
+#define DJI_GM2006_RPM_TO_OMEGA 0.0029088821f  // (1/60*2*pi/36) m2006(减速比36:1) (rpm)->(rad/s)
 
 typedef struct
 {
@@ -53,9 +50,10 @@ typedef struct
  */
 typedef struct
 {
-    /*状态量*/
+    /*电机信息*/
     const DJI_Motor_Measure_t * motor_measure;  // 电机测量数据指针
 
+    /*状态量*/
     float accel;     //(rad/s^2)电机加速度
     float w;         //(rad/s)电机转速
     float v;         //(m/s)电机转速
@@ -65,6 +63,7 @@ typedef struct
     int16_t current_set;  // 电机发送电流
     int8_t direction;     // 电机旋转方向（1或-1）
 } DJI_Motor_s;
+
 
 /*-------------------- DM Motor --------------------*/
 
