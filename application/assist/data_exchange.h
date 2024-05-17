619c81d6773efba83b6e4d34df66000ab389d726
小企鹅 <1357482552@qq.com>
2024-05-17 14:50:13 +0800

    新增数据交换中心

---
+++ application/assist/data_exchange.h
@@ -0,0 +1,20 @@
+/**
+  ****************************(C) COPYRIGHT 2024 Polarbear****************************
+  * @file       data_exchange.c/h
+  * @brief      数据交换中心，用于各个模块之间的数据交换，不用相互调用头文件，加强各模块之间的解耦
+  * @history
+  *  Version    Date            Author          Modification
+  *  V1.0.0     May-16-2024     Penguin         1. 完成。
+  *
+  @verbatim
+  ==============================================================================
+
+  ==============================================================================
+  @endverbatim
+  ****************************(C) COPYRIGHT 2024 Polarbear****************************
+  */
+
+#ifndef __DATA_EXCHANGE_H
+#define __DATA_EXCHANGE_H
+
+#endif  // __DATA_EXCHANGE_H
