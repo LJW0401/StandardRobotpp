2d9d17bc453329d53554b2c42069c00043f3f32c
小企鹅 <1357482552@qq.com>
2024-05-20 13:58:53 +0800

    添加蜂鸣器音符发声

---
+++ application/music/music_you.c
@@ -0,0 +1,5 @@
+
+#include "music_you.h"
+#include "music.h"
+#include "bsp_buzzer.h"
+
--- bsp/boards/bsp_buzzer.c
+++ bsp/boards/bsp_buzzer.c
@@ -11,3 +11,24 @@ void buzzer_off(void)
 {
     __HAL_TIM_SetCompare(&htim4, TIM_CHANNEL_3, 0);
 }
+
+void buzzer_note(uint16_t note)
+{
+    // 禁用定时器
+    __HAL_TIM_DISABLE(&htim4);
+    
+    // 重置定时器计数器
+    htim4.Instance->CNT = 0;
+    
+    // 设置自动重装载寄存器（ARR），以控制PWM信号的频率
+    htim4.Instance->ARR = (21000 / note - 1) * 1u;
+    
+    // 设置比较寄存器（CCR3），以控制PWM信号的占空比
+    htim4.Instance->CCR3 = (10500 / note - 1) * 1u;
+    
+    // 重新启用定时器
+    __HAL_TIM_ENABLE(&htim4);
+    
+    // 启动PWM信号
+    HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+}
