d00a494c3ed263cbaccb8ed5a78929b484d3fbde
是小企鹅呀 <1357482552@qq.com>
2024-05-27 22:02:05 +0800

    添加自定义控制器相关的硬件定义

---
+++ application/robot_param_penguin_mini_custom_controller.h
@@ -0,0 +1,19 @@
+/**
+  * @file       robot_param_penguin_mini_custom_controller.h
+  * @brief      这里是小企鹅的迷你自定义控制器的参数配置文件，包括物理参数、PID参数等
+  */
+
+#ifndef INCLUDED_ROBOT_PARAM_H
+#define INCLUDED_ROBOT_PARAM_H
+#include "robot_typedef.h"
+
+#define CHASSIS_TYPE CHASSIS_NONE                              // 选择底盘类型
+#define GIMBAL_TYPE GIMBAL_NONE                                // 选择云台类型
+#define SHOOT_TYPE SHOOT_NONE                                  // 选择发射机构类型
+#define MECHANICAL_ARM_TYPE MECHANICAL_ARM_NONE                // 选择机械臂类型
+#define CUSTOM_CONTROLLER_TYPE CUSTOM_CONTROLLER_PENGUIN_MINI  // 选择自定义控制器类型
+#define CONTROL_TYPE CHASSIS_AND_GIMBAL                        // 选择控制类型
+
+/*-------------------- Custom controller --------------------*/
+
+#endif /* INCLUDED_ROBOT_PARAM_H */
