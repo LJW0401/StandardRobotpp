23d1487595e689b51862aacbd1f69784ebd27e91
是小企鹅呀 <1357482552@qq.com>
2024-06-13 20:41:34 +0800

    照着裁判系统的抄了一个UART2的中断处理函数

--- bsp/boards/bsp_uart.c
+++ bsp/boards/bsp_uart.c
@@ -4,6 +4,17 @@
  */
 #include "bsp_uart.h"
 
+#include "CRC8_CRC16.h"
+#include "fifo.h"
+
+/**
+ * @brief 使用uart发送数据
+ * @param huart 
+ * @param pData 
+ * @param Size 
+ * @param Timeout 
+ * @return 
+ */
 UartSendState_e UartSendTxMessage(
     UART_HandleTypeDef * huart, uint8_t * pData, uint16_t Size, uint32_t Timeout)
 {
@@ -17,4 +28,40 @@ UartSendState_e UartSendTxMessage(
         return UART_SEND_OK;
     }
     return UART_SEND_FAIL;
+}
+
+#define UART2_RX_BUF_LENGHT 512
+#define UART2_FIFO_BUF_LENGTH 1024
+
+uint8_t  UART2_BUFFER[2][UART2_RX_BUF_LENGHT];
+fifo_s_t UART2_FIFO;
+uint8_t  UART2_FIFO_BUFFER[UART2_FIFO_BUF_LENGTH];
+// unpack_data_t referee_unpack_obj;
+
+void USART1_IRQHandler(void)
+{
+    static volatile uint8_t res;
+    if (USART1->SR & UART_FLAG_IDLE) {
+        __HAL_UART_CLEAR_PEFLAG(&huart1);
+
+        static uint16_t this_time_rx_len = 0;
+
+        if ((huart1.hdmarx->Instance->CR & DMA_SxCR_CT) == RESET) {
+            __HAL_DMA_DISABLE(huart1.hdmarx);
+            this_time_rx_len = UART2_RX_BUF_LENGHT - __HAL_DMA_GET_COUNTER(huart1.hdmarx);
+            __HAL_DMA_SET_COUNTER(huart1.hdmarx, UART2_RX_BUF_LENGHT);
+            huart1.hdmarx->Instance->CR |= DMA_SxCR_CT;
+            __HAL_DMA_ENABLE(huart1.hdmarx);
+            fifo_s_puts(&UART2_FIFO, (char *)UART2_BUFFER[0], this_time_rx_len);
+            // detect_hook(REFEREE_TOE);
+        } else {
+            __HAL_DMA_DISABLE(huart1.hdmarx);
+            this_time_rx_len = UART2_RX_BUF_LENGHT - __HAL_DMA_GET_COUNTER(huart1.hdmarx);
+            __HAL_DMA_SET_COUNTER(huart1.hdmarx, UART2_RX_BUF_LENGHT);
+            huart1.hdmarx->Instance->CR &= ~(DMA_SxCR_CT);
+            __HAL_DMA_ENABLE(huart1.hdmarx);
+            fifo_s_puts(&UART2_FIFO, (char *)UART2_BUFFER[1], this_time_rx_len);
+            // detect_hook(REFEREE_TOE);
+        }
+    }
 }
